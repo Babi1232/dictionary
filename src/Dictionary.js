198ea4694c600aff1ebd5ebd2a28ebba9b15a378
babisha <Babisha.shrestha1995@gmail.com>
2022-08-14 14:39:45 +0200

    added meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -23,6 +24,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
